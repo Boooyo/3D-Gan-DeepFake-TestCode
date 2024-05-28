1480a15c8d386365a87504767fcc6e3549f4cf81
Boooyo <136545068+Boooyo@users.noreply.github.com>
2024-05-28 09:38:11 +0900

    코드 수정

--- Model/generator.py
+++ Model/generator.py
@@ -2,22 +2,31 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 def build_generator():
-    model = tf.keras.Sequential()
-    model.add(layers.Dense(128 * 4 * 4 * 4, activation="relu", input_dim=100))
-    model.add(layers.Reshape((4, 4, 4, 128)))
+    noise_dim = 100
+    init_shape = (4, 4, 4, 128)  # Dense 레이어 이후 초기 모양
+
+    model = tf.keras.Sequential(name='Generator')
     
-    model.add(layers.Conv3DTranspose(128, (4, 4, 4), strides=(2, 2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
+    # 완전 연결층: 입력 노이즈를 투영하고 재구성
+    model.add(layers.Dense(tf.math.reduce_prod(init_shape), activation="relu", input_dim=noise_dim))
+    model.add(layers.Reshape(init_shape))
+
+    # Deconvolution (Conv3DTranspose) 레이어
+    filters = [128, 64, 32, 1]
+    kernel_size = (4, 4, 4)
+    strides = (2, 2, 2)
+    padding = 'same'
     
-    model.add(layers.Conv3DTranspose(64, (4, 4, 4), strides=(2, 2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
+    for i in range(len(filters) - 1):
+        model.add(layers.Conv3DTranspose(filters[i], kernel_size, strides=strides, padding=padding))
+        model.add(layers.BatchNormalization())
+        model.add(layers.ReLU())
     
-    model.add(layers.Conv3DTranspose(32, (4, 4, 4), strides=(2, 2, 2), padding='same'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.ReLU())
-    
-    model.add(layers.Conv3DTranspose(1, (4, 4, 4), strides=(2, 2, 2), padding='same', activation='tanh'))
+    # 출력 레이어
+    model.add(layers.Conv3DTranspose(filters[-1], kernel_size, strides=strides, padding=padding, activation='tanh'))
     
     return model
+
+# 생성기 모델 인스턴스화 및 구조 출력
+generator = build_generator()
+generator.summary()
